b746e3e287ec4c4aa12d9f809e218a262134a9d3
Edmond-Lam <elam3@stuy.edu>
2017-03-04 19:45:44 -0500

    grov

--- canvas.js
+++ canvas.js
@@ -9,9 +9,6 @@ var rid;
 
 var namespace = "http://www.w3.org/2000/svg";
 
-var img = new Image();
-img.src = "https://upload.wikimedia.org/wikipedia/en/thumb/1/18/Dvd-video-logo.svg/1280px-Dvd-video-logo.svg.png";
-
 svgImage.addEventListener("mousemove", function(e) {
     mousex = e.offsetX;
     mousey = e.offsetY;
@@ -20,11 +17,11 @@ svgImage.addEventListener("mousemove", function(e) {
 
 var drawDVD = function(x,y, imgW, imgH){
     var c = document.createElementNS(namespace, "image");
-    c.setAttribute("x", toString(x));
-    c.setAttribute("y", toString(y));
-    c.setAttribute("width", toString(imgW));
-    c.setAttribute("height", toString(imgH));
-    c.setAttributeNS('http://www.w3.org/1999/xlink','href','http://i.imgur.com/LQIsf.jpg');
+    c.setAttribute("x", String(x));
+    c.setAttribute("y", String(y));
+    c.setAttribute("width", String(imgW));
+    c.setAttribute("height", String(imgH));
+    c.setAttributeNS('http://www.w3.org/1999/xlink','href','https://upload.wikimedia.org/wikipedia/en/1/18/Dvd-video-logo.svg');
     svgImage.appendChild(c);
 }
 
@@ -32,15 +29,14 @@ var circleAnim = function() {
     var x = 25;
     var increase = true;
     window.cancelAnimationFrame(rid);
-    
     var drawDot = function(e){
 	while (svgImage.hasChildNodes()){
 	    svgImage.removeChild(svgImage.lastChild);
 	}
 	console.log(rid);
 	var circle = document.createElementNS(namespace, "circle")
-	circle.setAttribute("cx", "0");
-	circle.setAttribute("cy", "0");
+	circle.setAttribute("cx", String(width/2));
+	circle.setAttribute("cy", String(height/2));
 	circle.setAttribute("r", x.toString());
 	circle.setAttribute("fill", "black");
 	if (x >= 250) {
@@ -55,6 +51,7 @@ var circleAnim = function() {
 	else {
 	    x = x - 2;
 	}
+	svgImage.appendChild(circle);
 	rid = window.requestAnimationFrame( drawDot );
     };
 
@@ -67,29 +64,29 @@ var dvdAnim = function(){
     var yVel = 2;
     var x = width / 4 + Math.random() * width / 2;
     var y = height / 4 + Math.random() * height / 2;
-    var imgW = width/6;
-    var imgH = height/6;
+    var imgW = 210/2;
+    var imgH = 130/2;
     
     var anim = function(){
 	
-	clrScreen();
+	stopAll();
 	x += xVel;
 	y += yVel;
 	
 	if (xVel >= 0 && x + imgW >= width){
-	    xVel = -4;
+	    xVel = -3;
 	}
 	if (xVel <= 0 && x <= 0){
-	    xVel = 4;
+	    xVel = 3;
 	}
 	if (yVel >= 0 && y + imgH >= height){
-	    yVel = -3;
+	    yVel = -2;
 	}
 	if (yVel <= 0 && y <= 0){
-	    yVel = 3;
+	    yVel = 2;
 	}
 	
-	drawDVD(x,y,imgW, imgH);
+	drawDVD(String(x),String(y),String(imgW), String(imgH));
 	
 	rid = window.requestAnimationFrame(anim);
 	
@@ -101,6 +98,7 @@ var dvdAnim = function(){
 }
 
 var stopAll = function(){
+    
     while (svgImage.hasChildNodes()){
 	svgImage.removeChild(svgImage.lastChild);
     }
